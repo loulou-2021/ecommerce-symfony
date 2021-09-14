56ef9f2ed8f15df5f1c892d08e626e417c5f6029
Matthieu Mota <matthieumota@gmail.com>
2021-09-14 12:59:40 +0200

    Form in class

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,9 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\CountryType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,12 +21,7 @@ class ContactController extends AbstractController
         dump($contact);
 
         // On crée un formulaire avec un Symfony
-        $form = $this->createFormBuilder($contact)
-            ->add('name')
-            ->add('email')
-            ->add('message', TextareaType::class)
-            ->add('country', CountryType::class)
-            ->getForm();
+        $form = $this->createForm(ContactType::class, $contact);
 
         // On va faire le lien entre notre formulaire et les donnée de la requête
         $form->handleRequest($request);
@@ -39,6 +33,10 @@ class ContactController extends AbstractController
             // On peut récupérer les données du formulaire
             // dump($form->getData());
             // dump($contact);
+            // Insérer en BDD... Persister un objet avec Doctrine
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($contact); // Mets de côté l'objet
+            $manager->flush(); // INSERT
         }
 
         return $this->render('contact/index.html.twig', [
---
+++ src/Form/ContactType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('message', TextareaType::class)
+            ->add('country', CountryType::class)
+            ->add('askedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
