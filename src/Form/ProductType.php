b32ee03ba56fb5045c2851fc95951a7a1cdf47a2
Matthieu Mota <matthieumota@gmail.com>
2021-09-15 10:07:08 +0200

    Finish tp and bonus

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,12 +14,12 @@ class ProductType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('slug')
-            ->add('description')
+            // ->add('slug')
+            ->add('description', TextareaType::class)
             ->add('price')
-            ->add('createdAt')
+            // ->add('createdAt')
             ->add('liked')
-            ->add('image')
+            // ->add('image')
             ->add('promotion')
         ;
     }
